c623c31ab38f9d2359ec2155c7ccfbf7f859df85
Ismael Medina <104896885+DarkFlame02@users.noreply.github.com>
2024-11-21 13:20:52 +0000

    cosas

---
+++ src/main/java/dad/hipoteca/api/HipotecaApi.java
@@ -0,0 +1,45 @@
+package dad.hipoteca.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class HipotecaApi {
+    private static final String BASE_URL = "https://openlibrary.org/";
+
+    private final HipotecaInterface service;
+
+    public HipotecaApi() {
+
+        ConnectionPool pool = new ConnectionPool(1, 5, TimeUnit.SECONDS);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectionPool(pool)
+                .build();
+
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
+                .build();
+
+        service = retrofit.create(HipotecaInterface.class);
+
+    }
+
+
+
+}
+
+
---
+++ src/main/java/dad/hipoteca/api/HipotecaInterface.java
@@ -0,0 +1,13 @@
+package dad.hipoteca.api;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface HipotecaInterface {
+
+    // localhost:3000/hipoteca?capital=1000&intereses=5&años=2
+    @GET("search.json")
+    Call<HipotecaApi> search(@Query("q") String query);
+
+}
